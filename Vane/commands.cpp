f498bc37225d8709bfa77fe06f7a318611bb24cb
bluebear94 <tkook11@gmail.com>
2017-04-01 04:57:46 -0400

    hAI Dis are an COMMIT!!! it r sO FUNNY!!!

--- Vane/commands.cpp
+++ Vane/commands.cpp
@@ -1,20 +1,41 @@
 #include "commands.h"
 
+#include <time.h>
+
+bool isAprilFools() {
+  time_t now = std::time(nullptr);
+  std::tm* nowTM = localtime(&now);
+  return nowTM->tm_mon == 3 && nowTM->tm_mday == 1;
+}
+
 static std::string INFO =
   "I am VaneBot, written by Uruwi.\n"
   "I was created for use in the AGC Discord server.\n"
-  "I am programmed in C++, using Aidoboy's library \\< https://github.com/Aidoboy/discordpp \\>."
+  "I am programmed in C++, using Aidoboy's library \\< https://github.com/Aidoboy/discordpp \\>.\n"
+  "Check out my code at \\< https://github.com/nagakawa/vane \\>."
+  ;
+static std::string INFO_APRIL =
+  "hAI me Iz vANeBot! me iZ oohrUDEwhee's dAUgHter!!!\n"
+  "oOhRudeWHEE :spoon:ed oNE DaY iN le BEEGEEVEE SERVER!!! OMG!!!\n"
+  "lolololol HAHA Me iz progRAMMed in SEE PLUS PLUS!!! OMG kan u believes it???\n"
+  "kthxbye"
   ;
 
 void respondInfo(discordpp::Bot* bot, nlohmann::json response) {
   (void) bot;
   std::string sid = response["d"]["channel_id"];
   auto id = std::stoull(sid);
-  discordpp::DiscordAPI::channels::messages::create(id, INFO);
+  discordpp::DiscordAPI::channels::messages::create(
+    id,
+    isAprilFools() ? INFO_APRIL : INFO
+  );
 }
 
 std::string commandString() {
-  std::string message = "Here is a list of my commands:\n```";
+  std::string message =
+    isAprilFools() ?
+    "sAY These things too meh!!!1111\n```" :
+    "Here is a list of my commands:\n```";
   for (const auto& p : customCommands) {
     message += p.first;
     message += '\n';
