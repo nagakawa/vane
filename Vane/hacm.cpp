8a3523d6b3cecce20904f7e5efb4ff0a4f785179
bluebear94 <uruwi@protonmail.com>
2017-09-06 14:07:21 -0400

    More symbols

--- Vane/hacm.cpp
+++ Vane/hacm.cpp
@@ -9,8 +9,22 @@ static const char* lowercaseHacm[] = {
   "ɑ", "φ", "h", "c", "s",
   "⎫", "l", "ᴅ", "n", "ɔ",
   "d", "ɜ", "µ", "ȷ", "ſ",
-  "ə", "u", "o", "╰", "ɥ",
+  "ə", "u", "o", "Ɩ", "ɥ",
   "z"
+};
+
+static const char* lowercaseHacmSuper[] = {
+  "ˈ", "ᵇ", "?", "?", "ᵉ",
+  "ᵅ", "ᵠ", "ʰ", "ᶜ", "ˢ",
+  "?", "ˡ", "ᴰ", "ⁿ", "ᵓ",
+  "ᵈ", "ᶟ", "?", "ʲ", "ᶴ",
+  "ᵊ", "ᵘ", "ᵒ", "ᶥ", "ᶣ",
+  "ᶻ"
+};
+
+static const char* hacmDigits[] = {
+  "0", "1", "⅃", "ʔ", "Ƣ",
+  "4", "ʕ", "9", "Δ", "L"
 };
 
 std::string latinToHacm(const std::string &l) {
@@ -19,8 +33,20 @@ std::string latinToHacm(const std::string &l) {
   size_t len = l.length();
   while (i < len) {
     char c = l[i];
-    if (islower(c)) {
+    if (c == '\\') {
+      s += l[++i];
+    } else if (c == '^') {
+      char d = l[++i];
+      if (islower(d)) {
+        s += lowercaseHacmSuper[d - 'a'];
+      } else {
+        s += '^';
+        s += d;
+      }
+    } else if (islower(c)) {
       s += lowercaseHacm[c - 'a'];
+    } else if (isdigit(c)) {
+      s += hacmDigits[c - '0'];
     } else {
       s += c;
     }
